cbf442277cb75e92f4b66bd031d977d3e5ea990e
Huzaifa Elahi <93325659+Huzaifa-Elahi@users.noreply.github.com>
2022-07-04 03:12:11 +0500

    Update form_handler.php
    tweaked a bit

--- form_handler.php
+++ form_handler.php
@@ -4,17 +4,20 @@ $visitor_email =$_POST['email'];
 $subject =$_POST['subject'];
 $comment =$_POST['comment'];
 
-$email_from = '';
+$email_from = 'https://huzaifa-elahi.github.io/Univeristy.github.io/index.html';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                   "Subject: $subject.\n".
                     "User Message: $comment.\n";
 
-$to ="yasirkhan2134567890@gmail.com";         
+// $to ="yasirkhan2134567890@gmail.com";  
+
+// $to ="
+$to ="yasirkhan2134567890@gmail.com";  ;  
 
 $header = "From: $email_from \r\n";
-$header = "Reply-To: $visitor_email \r\n";
+$header .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 header("Location: contact.html");
